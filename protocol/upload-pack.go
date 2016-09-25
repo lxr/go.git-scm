834c19f657e0cbeef056f07d518f8b9ab288b07c
Lari Rasku <lari.o.rasku@gmail.com>
2016-09-25 20:30:21 +0300

    protocol: UploadPack: implement packing heuristics
    writePack now sorts its objects first by type and then by size before
    writing them out into the packfile.  This is almost the same as the
    heuristics used by the reference implementation [1], with the important
    exception that blobs and trees aren't sorted by their base names.
    This is unfortunate, as files with the same name are likely to delta
    extremely well against each other, but I wasn't willing to implement
    the preprocessing and bookkeeping data necessary for this.
    
    Sorting by type is not strictly necessary in our implementation, as
    packfile.Writer maintains a separate delta window (of one object)
    for each object type; but it helps avoid pointless size comparisons,
    and may come in useful if the implementation ever changes.
    
    [1]: https://www.kernel.org/pub/software/scm/git/docs/technical/pack-heuristics.html

--- protocol/upload-pack.go
+++ protocol/upload-pack.go
@@ -3,6 +3,7 @@ package protocol
 import (
 	"fmt"
 	"io"
+	"sort"
 
 	"github.com/lxr/go.git-scm/object"
 	"github.com/lxr/go.git-scm/packfile"
@@ -105,11 +106,73 @@ func negotiate(repo repository.Interface, pktw *pktline.Writer, pktr *pktline.Re
 	}
 }
 
+// objectSizeOf returns an approximation of the given object's binary
+// representation size.  It returns -1 if the object is not one of the
+// standard Git types.
+func objectSizeOf(obj object.Interface) int {
+	switch obj := obj.(type) {
+	case *object.Commit:
+		n := 6 + 40
+		for range obj.Parent {
+			n += 8 + 40
+		}
+		n += 28 + len(obj.Author.Name) + len(obj.Author.Email)
+		n += 31 + len(obj.Committer.Name) + len(obj.Committer.Email)
+		n += 1 + len(obj.Message)
+		return n
+	case object.Tree:
+		n := 0
+		for filename := range obj {
+			n += 28 + len(filename)
+		}
+		return n
+	case *object.Blob:
+		return len(*obj)
+	case *object.Tag:
+		n := 8 + 40
+		n += 6 + len(obj.Type.String())
+		n += 5 + len(obj.Tag)
+		n += 28 + len(obj.Tagger.Name) + len(obj.Tagger.Email)
+		n += 1 + len(obj.Message)
+		return n
+	default:
+		return -1
+	}
+}
+
+// objSlice is a wrapper type for sorting Git object slices by type
+// and size.  Packfiles written in this order compress better.
+type objSlice []object.Interface
+
+func (objs objSlice) Len() int {
+	return len(objs)
+}
+
+func (objs objSlice) Less(i, j int) bool {
+	a := objs[i]
+	b := objs[j]
+	aType := object.TypeOf(a)
+	bType := object.TypeOf(b)
+	switch {
+	case aType < bType:
+		return true
+	case aType > bType:
+		return false
+	default: // aType == bType
+		return objectSizeOf(a) >= objectSizeOf(b)
+	}
+}
+
+func (objs objSlice) Swap(i, j int) {
+	objs[i], objs[j] = objs[j], objs[i]
+}
+
 func writePack(w io.Writer, objs []object.Interface) error {
 	pfw, err := packfile.NewWriter(w, int64(len(objs)))
 	if err != nil {
 		return err
 	}
+	sort.Sort(objSlice(objs))
 	for _, obj := range objs {
 		if err := pfw.WriteObject(obj); err != nil {
 			return err
