edd944be1ba9214813ee6f18673c02acc80bc166
Lari Rasku <lari.o.rasku@gmail.com>
2016-09-28 20:53:24 +0300

    repository/appengine: document schema
    The majority of this schema is amusingly implemented in package
    object, but this is the better place to document it.

--- repository/appengine/appengine.go
+++ repository/appengine/appengine.go
@@ -1,8 +1,18 @@
 // Package appengine implements a Git repository backed by the Google
-// App Engine datastore and memcache.
+// App Engine datastore and memcache.  See the documentation for the
+// InitRepository function for how the repository is stored in the
+// datastore.
 package appengine
 
-// TODO(lor): Document how Git objects are stored in the datastore.
+// BUG(lor): The datastore limits
+// (https://cloud.google.com/datastore/docs/concepts/limits) apply to
+// package appengine too, which restricts its usefulness as a large-file
+// repository backend.
+
+// BUG(lor): The datastore schema does not distinguish objects between
+// repositories; all objects effectively belong to the same giant object
+// store.  Only reachability from refs denotes "membership" in a
+// repository.
 
 import (
 	"golang.org/x/net/context"
@@ -19,10 +29,62 @@ func mapErr(err error) error {
 	return err
 }
 
-// InitRepository initializes a new Git repository structure in the App
-// Engine datastore using the given context and rooted at the given key.
-// The prefix string is used to "namespace" entity kinds and memcache
-// accesses by prepending it to kind names and memcache keys.
+// InitRepository initializes a new Git repository in the App Engine
+// datastore using the given context.  A repository is stored in the
+// datastore using six kinds of entities: HEADs, refs, commits, trees,
+// blobs and tags, with the following schema:
+//
+// 	// actual kind depends on the kind of the root key:
+// 		HEAD (string)
+//
+// 	<prefix>ref:
+// 		ID (string)
+//
+// 	<prefix>commit:
+// 		Tree (string) // object ID
+// 		Parent (list(string)) // object IDs
+// 		Author.Date (datetime)
+// 		Author.Email (string)
+// 		Author.Name (string)
+// 		Author.TZ (int) // offset from GMT in seconds
+// 		Committer.Date (datetime)
+// 		Committer.Email (string)
+// 		Committer.Name (string)
+// 		Committer.TZ (int) // offset from GMT in seconds
+// 		Message (Text) // not indexed
+//
+// 	<prefix>tree:
+// 		Name (list(string))
+// 		Mode (list(int))
+// 		Object (list(string)) // object IDs
+//
+// 	<prefix>blob:
+// 		Contents (Blob) // not indexed
+//
+// 	<prefix>tag:
+// 		Object (string) // object ID
+// 		Type (string)
+// 		Tag (string)
+// 		Tagger.Date (datetime)
+// 		Tagger.Email (string)
+// 		Tagger.Name (string)
+// 		Tagger.TZ (int) // offset from GMT in seconds
+// 		Message (Text) // not indexed
+//
+// The prefix string is prepended to each kind name in order to provide
+// a means of avoiding kind name collisions with other applications
+// using the datastore.  It is similarly prepended to each memcache
+// key to avoid memcache collisions.
+//
+// In addition to storing the repository HEAD, the root key is used as
+// the parent of all ref keys, whose names are the names of the refs.
+//
+// For performance, all objects are stored without a parent key (i.e.
+// without an entity group).  The name of an object's key is the
+// hexadecimal representation of its ID.
+//
+// InitRepository does not clear already initialized repos; it merely
+// sets the HEAD to point to refs/heads/master.
 func InitRepository(ctx context.Context, root *datastore.Key, prefix string) (repository.Interface, error) {
 	r := &repo{
 		ctx:    ctx,
@@ -33,9 +95,9 @@ func InitRepository(ctx context.Context, root *datastore.Key, prefix string) (re
 }
 
 // OpenRepository returns a Git repository interface to the App Engine
-// datastore using the given context and rooted at the given key.  The
-// prefix string is used to "namespace" entity kinds and memcache
-// accesses by prepending it to kind names and memcache keys.
+// datastore using the given parameters.  Refer to the documentation for
+// the InitRepository function to see how they control access to the
+// repository.
 func OpenRepository(ctx context.Context, root *datastore.Key, prefix string) repository.Interface {
 	return &repo{
 		ctx:    ctx,
